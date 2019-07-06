68ebffe2b899cc0901de2e7b946c429447c2937e
Maxikk26 <maximiliano.bogo@gmail.com>
2019-07-06 17:47:57 -0400

    UI_Apuestas arreglos

--- Unity/Truco/Assets/Scripts/Interfaz/UI_Apuestas.cs
+++ Unity/Truco/Assets/Scripts/Interfaz/UI_Apuestas.cs
@@ -6,37 +6,36 @@ using System;
 using UnityEngine.UI;
 
 public class UI_Apuestas : MonoBehaviour{
-    private Button BtnTruco;
+    public Button BtnTruco,BtnTrucoSi,BtnTrucoNo,BtnEnvido,BtnEnvidoSi,BtnEnvidoNo;
+
     private bool cond;
-    public Button BtnTrucoSi;
     private string trucoActual = "Normal";
 
     void Start(){
-        Button BtnTruco = GameObject.Find("BtnTruco").GetComponent<Button>();
-
+        BtnTruco = GameObject.Find("BtnTruco").GetComponent<Button>();
         BtnTruco.interactable = false;
         BtnTruco.GetComponentInChildren<Text>().text = "Truco";
         set_string_BtnTruco("Truco");
 
-        Button BtnEnvido = GameObject.Find("BtnEnvido").GetComponent<Button>();
+        BtnEnvido = GameObject.Find("BtnEnvido").GetComponent<Button>();
         BtnEnvido.GetComponentInChildren<Text>().text = "Envido";
         BtnEnvido.interactable = false;
 
-        Button BtnTrucoSi = GameObject.Find("BtnTrucoSi").GetComponent<Button>();
+        BtnTrucoSi = GameObject.Find("BtnTrucoSi").GetComponent<Button>();
         BtnTrucoSi.GetComponentInChildren<Text>().text = "Si";
         BtnTrucoSi.interactable = false;
         BtnTrucoSi.onClick.AddListener(subirApuesta);
 
-        Button BtnTrucoNo = GameObject.Find("BtnTrucoNo").GetComponent<Button>();
+        BtnTrucoNo = GameObject.Find("BtnTrucoNo").GetComponent<Button>();
         BtnTrucoNo.GetComponentInChildren<Text>().text = "No";
         BtnTrucoNo.onClick.AddListener(reiniciarApuesta);
         BtnTrucoNo.interactable = false;
 
-        Button BtnEnvidoSi = GameObject.Find("BtnEnvidoSi").GetComponent<Button>();
+        BtnEnvidoSi = GameObject.Find("BtnEnvidoSi").GetComponent<Button>();
         BtnEnvidoSi.GetComponentInChildren<Text>().text = "Si";
         BtnEnvidoSi.interactable = false;
 
-        Button BtnEnvidoNo = GameObject.Find("BtnEnvidoNo").GetComponent<Button>();
+        BtnEnvidoNo = GameObject.Find("BtnEnvidoNo").GetComponent<Button>();
         BtnEnvidoNo.GetComponentInChildren<Text>().text = "No";
         BtnEnvidoNo.interactable = false;
 
@@ -85,62 +84,62 @@ public class UI_Apuestas : MonoBehaviour{
     }
 
     public void activarTruco(){
-        Button BtnTruco = GameObject.Find("BtnTruco").GetComponent<Button>();
+        BtnTruco = GameObject.Find("BtnTruco").GetComponent<Button>();
         BtnTruco.interactable = true;
     }
 
     public void activarBtnTrucoSi(){
-        Button BtnTrucoSi = GameObject.Find("BtnTrucoSi").GetComponent<Button>();
+        BtnTrucoSi = GameObject.Find("BtnTrucoSi").GetComponent<Button>();
         BtnTrucoSi.interactable = true;
     }
 
     public void activarBtnTrucoNo(){
-        Button BtnTrucoNo = GameObject.Find("BtnTrucoNo").GetComponent<Button>();
+        BtnTrucoNo = GameObject.Find("BtnTrucoNo").GetComponent<Button>();
         BtnTrucoNo.interactable = true;
     }
 
     public void desactivarTruco(){
-        Button BtnTruco = GameObject.Find("BtnTruco").GetComponent<Button>();
+        BtnTruco = GameObject.Find("BtnTruco").GetComponent<Button>();
         BtnTruco.interactable = false;
     }
 
     public void desactivarBtnTrucoSi(){
-        Button BtnTrucoSi = GameObject.Find("BtnTrucoSi").GetComponent<Button>();
+        BtnTrucoSi = GameObject.Find("BtnTrucoSi").GetComponent<Button>();
         BtnTrucoSi.interactable = false;
     }
 
     public void desactivarBtnTrucoNo(){
-        Button BtnTrucoNo = GameObject.Find("BtnTrucoNo").GetComponent<Button>();
+        BtnTrucoNo = GameObject.Find("BtnTrucoNo").GetComponent<Button>();
         BtnTrucoNo.interactable = false;
     }
 
     public void activarEnvido(){
-        Button BtnEnvido = GameObject.Find("BtnEnvido").GetComponent<Button>();
+        BtnEnvido = GameObject.Find("BtnEnvido").GetComponent<Button>();
         BtnEnvido.interactable = true;
     }
 
     public void activarBtnEnvidoSi(){
-        Button BtnEnvidoSi = GameObject.Find("BtnEnvidoSi").GetComponent<Button>();
+        BtnEnvidoSi = GameObject.Find("BtnEnvidoSi").GetComponent<Button>();
         BtnEnvidoSi.interactable = true;
     }
 
     public void activarBtnEnvidoNo(){
-        Button BtnEnvidoNo = GameObject.Find("BtnEnvidoNo").GetComponent<Button>();
+        BtnEnvidoNo = GameObject.Find("BtnEnvidoNo").GetComponent<Button>();
         BtnEnvidoNo.interactable = true;
     }
 
     public void desactivarEnvido(){
-        Button BtnEnvido = GameObject.Find("BtnEnvido").GetComponent<Button>();
+        BtnEnvido = GameObject.Find("BtnEnvido").GetComponent<Button>();
         BtnEnvido.interactable = false;
     }
 
     public void desactivarBtnEnvidoSi(){
-        Button BtnEnvidoSi = GameObject.Find("BtnEnvidoSi").GetComponent<Button>();
+        BtnEnvidoSi = GameObject.Find("BtnEnvidoSi").GetComponent<Button>();
         BtnEnvidoSi.interactable = false;
     }
 
     public void desactivarBtnEnvidoNo(){
-        Button BtnEnvidoNo = GameObject.Find("BtnEnvidoNo").GetComponent<Button>();
+        BtnEnvidoNo = GameObject.Find("BtnEnvidoNo").GetComponent<Button>();
         BtnEnvidoNo.interactable = false;
     }
 
