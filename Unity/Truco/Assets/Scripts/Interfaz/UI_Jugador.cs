f240ebd9b8bf75196be74b07aa30643027787e55
Roberto Carbajales <robertocd98@gmail.com>
2019-07-07 00:37:51 -0400

    UI_Jugador, UI_Mesa, Mueve Cartas, Asigna Cartas

--- Unity/Truco/Assets/Scripts/Interfaz/UI_Jugador.cs
+++ Unity/Truco/Assets/Scripts/Interfaz/UI_Jugador.cs
@@ -5,33 +5,92 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UI_Jugador : MonoBehaviour{
-    public Text turno;
-    public Text flor;
+    
+    public GameObject cartaPrefab;
+    public GameObject jugador; 
+    public GameObject goCarta1;
+    public GameObject goCarta2;
+    public GameObject goCarta3;
+
+    public Vector3 posicion;
+
     public UI_Carta cartaJugada;
     public UI_Carta carta1;
     public UI_Carta carta2;
     public UI_Carta carta3;
 
-    public void jugarCarta( int cartaJugada ){
+    void Start() {
+        
+    }
+
+    public void instanciarCarta(float x, float y, int carta, string pinta, int numero) {
+        Debug.Log("UI_Jugador.instanciarCarta()");
+        switch(carta) {
+            case 1: goCarta1 = Instantiate(cartaPrefab);
+                    goCarta1.transform.SetParent(jugador.transform);
+                    posicion = jugador.transform.position;
+                    posicion.x += x;
+                    posicion.y += y;
+                    goCarta1.transform.position = posicion;
+                    carta1 = goCarta1.GetComponent<UI_Carta>();
+                    carta1.darValor(pinta, numero);
+                    carta1.mostrarCarta();
+                    break;
+            case 2: goCarta2 = Instantiate(cartaPrefab);
+                    goCarta2.transform.SetParent(jugador.transform);
+                    posicion = jugador.transform.position;
+                    posicion.x += x;
+                    posicion.y += y;
+                    goCarta2.transform.position = posicion;
+                    carta2 = goCarta2.GetComponent<UI_Carta>();
+                    carta2.darValor(pinta, numero);
+                    carta2.mostrarCarta();
+                    break;
+            case 3: goCarta3 = Instantiate(cartaPrefab);
+                    goCarta3.transform.SetParent(jugador.transform);
+                    posicion = jugador.transform.position;
+                    posicion.x += x;
+                    posicion.y += y;
+                    goCarta3.transform.position = posicion;
+                    carta3 = goCarta3.GetComponent<UI_Carta>();
+                    carta3.darValor(pinta, numero);
+                    carta3.mostrarCarta();
+                    break;
+        }
+    }
+
+    public void jugarCarta( int carta, float x, float y ){
         // cartaJugada e { 1 , 2 , 3 }
+        Debug.Log("UI_Jugador.jugarCarta()");
+        switch(carta) {
+            case 1: posicion.x += x;
+                    posicion.y += y;
+                    goCarta1.transform.position = posicion;
+                    break;
+            case 2: posicion.x += x;
+                    posicion.y += y;
+                    goCarta2.transform.position = posicion;
+                    break;
+            case 3: posicion.x += x;
+                    posicion.y += y;
+                    goCarta3.transform.position = posicion;
+                    break;
+        }
     }
-
-    public void activarTurno(){
-        /*
-            ponerle color rojo a turno
-            activar las cartas para poder usar
-        */
+    /* public void desactivarTurno() {
+        Debug.Log("UI_Jugador.desactivarTurno()");
+        btnTurno.interactable = false;
     }
-
-    public void desactivarTurno(){
-
+    public void activarTurno() {
+        Debug.Log("UI_Jugador.activarTurno()");
+        btnTurno.interactable = true;
     }
-
-    public void activarFlor(){
-
+    public void desactivarFlor() {
+        Debug.Log("UI_Jugador.desactivarFlor()");
+        btnFlor.interactable = false;
     }
-
-    public void desactivarFlor(){
-
-    }
+    public void activarFlor() {
+        Debug.Log("UI_Jugador.activarFlor()");
+        btnFlor.interactable = true;
+    } */
 }
---
+++ Unity/Truco/Assets/Scripts/Interfaz/UI_Vira.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Vira : MonoBehaviour {
+    
+    public string pinta;   // { O, B, E, C }
+    public int numero;     // { 1, 2 ... 12 } - { 8, 9 }
+
+    void Start(){
+        
+    }
+
+    public void darValor( string pinta, int numero ){
+        Debug.Log("UI_Carta.darValor( " + pinta + " , " + numero + " )");
+        this.pinta = pinta;
+        this.numero = numero;
+        Image imagenCarta = gameObject.GetComponent<Image>();
+        imagenCarta.sprite = Resources.Load<Sprite>("Sprites/" + pinta + numero);
+    }
+
+}
