bdc59852b2ca317390048642165f7cbc9285bd15
Maxikk26 <maximiliano.bogo@gmail.com>
2019-07-07 10:52:42 -0400

    cambio interfaz

--- Unity/Truco/Assets/Scripts/Interfaz/Interfaz.cs
+++ Unity/Truco/Assets/Scripts/Interfaz/Interfaz.cs
@@ -6,10 +6,12 @@ public class Interfaz : MonoBehaviour {
     public UI_Mesa mesa;
 	public UI_Carta carta;
 	public UI_Apuestas apuesta;
+	public UI_Info info;
     public UI_Comunicacion comunicacion;
 
 
 	void Awake(){
         mesa = GameObject.Find("Mesa").GetComponent<UI_Mesa>();
+		info = GameObject.Find("UI_Info").GetComponent<UI_Info>();
 	}
 }
